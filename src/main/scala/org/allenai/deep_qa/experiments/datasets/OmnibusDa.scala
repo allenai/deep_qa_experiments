739ea2684919ad7c9fa1377e7d60109ca67cada2
Nelson Liu <nelson.liu.2009@gmail.com>
2017-02-20 22:43:02 +0000

    add code to get the background passage for a DA question

--- src/main/scala/org/allenai/deep_qa/experiments/datasets/OmnibusDa.scala
+++ src/main/scala/org/allenai/deep_qa/experiments/datasets/OmnibusDa.scala
@@ -11,8 +11,9 @@ object omnibusDaDatasets {
 
   def omnibusDaFile(omnibusDaDir: String, grade: String, split: String): JValue = {
     val datasetGradePath = s"Omnibus-Gr${grade}-NDDA"
-    val outputDirectory = omnibusDaDir + datasetGradePath +"/processed/"
-    val inputFile = omnibusDaDir + datasetGradePath + s"/${datasetGradePath}-${split}.csv"
+    val outputDirectory = omnibusDaDir + datasetGradePath +s"/processed/${split}/"
+    val splitCapitalized = split.capitalize
+    val inputFile = omnibusDaDir + datasetGradePath + s"/${datasetGradePath}-${splitCapitalized}.csv"
     val outputFiles = Seq(outputDirectory + s"${split}.tsv")
     ("sentence producer type" -> "dataset reader") ~
       ("reader" -> "omnibus da") ~
@@ -30,13 +31,27 @@ object omnibusDaDatasets {
   // Train files
   val omnibusNDDA04TrainFile = omnibusDaFile(baseDir, "04", "train")
   val omnibusNDDA04Train = omnibusDaDataset(baseDir, "04", "train")
+  val omnibusNDDA04TrainBuscBackgroundFile = SciQDatasets.makePassageBackgroundFile(
+    omnibusNDDA04TrainFile,
+    "question and answer",
+    ScienceCorpora.buscElasticSearchIndex(3)
+  )
 
   // Dev Files
   val omnibusNDDA04DevFile = omnibusDaFile(baseDir, "04", "dev")
   val omnibusNDDA04Dev = omnibusDaDataset(baseDir, "04", "dev")
+  val omnibusNDDA04DevBuscBackgroundFile = SciQDatasets.makePassageBackgroundFile(
+    omnibusNDDA04DevFile,
+    "question and answer",
+    ScienceCorpora.buscElasticSearchIndex(3)
+  )
 
   // Test Files
   val omnibusNDDA04TestFile = omnibusDaFile(baseDir, "04", "test")
   val omnibusNDDA04Test = omnibusDaDataset(baseDir, "04", "test")
-
+  val omnibusNDDA04TestBuscBackgroundFile = SciQDatasets.makePassageBackgroundFile(
+    omnibusNDDA04TestFile,
+    "question and answer",
+    ScienceCorpora.buscElasticSearchIndex(3)
+  )
 }
