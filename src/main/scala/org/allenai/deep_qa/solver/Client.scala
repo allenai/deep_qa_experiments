a5a2343b4b4339bbe239f34161140d5ee2811102
Nelson Liu <nelson.liu.2009@gmail.com>
2017-03-07 20:18:38 +0000

    remove unnecessary span field from protobuf spec

--- src/main/scala/org/allenai/deep_qa/solver/Client.scala
+++ src/main/scala/org/allenai/deep_qa/solver/Client.scala
@@ -65,30 +65,30 @@ class Client(host: String, port: Int) extends LazyLogging {
             Seq()
           )
         })
-        MessageInstance(instanceType, questionText, answerOptions, backgroundInstances, Seq())
+        MessageInstance(instanceType, questionText, answerOptions, backgroundInstances)
       }
       case i: QuestionAnswerInstance => {
         val instanceType = InstanceType.QUESTION_ANSWER
         val questionText = i.question
         val answerOptions = i.answers
-        MessageInstance(instanceType, questionText, answerOptions, Seq(), Seq(), "", Seq())
+        MessageInstance(instanceType, questionText, answerOptions, Seq(), Seq(), "")
       }
       case i: MultipleTrueFalseInstance[_] => {
         val instanceType = InstanceType.MULTIPLE_TRUE_FALSE
         val containedInstances = i.instances.map(instanceToMessage)
-        MessageInstance(instanceType, "", Seq(), Seq(), containedInstances, "", Seq())
+        MessageInstance(instanceType, "", Seq(), Seq(), containedInstances, "")
       }
       case i: TrueFalseInstance => {
         val instanceType = InstanceType.TRUE_FALSE
         val questionText = i.statement
         val answerOptions = Seq()
-        MessageInstance(instanceType, questionText, answerOptions, Seq(), Seq(), "", Seq())
+        MessageInstance(instanceType, questionText, answerOptions, Seq(), Seq(), "")
       }
       case i: CharacterSpanInstance => {
         val instanceType = InstanceType.CHARACTER_SPAN
         val questionText = i.question
         val passageText = i.passage
-        MessageInstance(instanceType, questionText, Seq(), Seq(), Seq(), passageText, Seq())
+        MessageInstance(instanceType, questionText, Seq(), Seq(), Seq(), passageText)
       }
     }
   }
