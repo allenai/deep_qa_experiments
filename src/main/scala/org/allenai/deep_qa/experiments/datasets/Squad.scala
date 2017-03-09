6bfb9378d5adab676ce6f482c84faa79b20b3d44
Matt Gardner <mattg@allenai.org>
2017-03-09 10:25:49 -0800

    Added code to convert SQuAD data into a sequence tagging task (#13)
    * Added code to convert SQuAD data into a sequence tagging task
    
    * Fixed failing tests
    
    * Using exists instead of foldLeft

--- src/main/scala/org/allenai/deep_qa/experiments/datasets/Squad.scala
+++ src/main/scala/org/allenai/deep_qa/experiments/datasets/Squad.scala
@@ -23,10 +23,40 @@ object SquadDatasets {
     ("data files" -> List(file))
   }
 
+  def squadTaggingFile(
+    squadDir: String,
+    split: String,
+    version: String="1.1",
+    tokenize: Boolean=true
+  ): JValue = {
+    val outputDirectory = squadDir + "processed/"
+    val inputFile = squadDir + s"${split}-v${version}.json"
+    val outputFiles = Seq(outputDirectory + s"tagging_${split}.tsv")
+    ("sentence producer type" -> "dataset reader") ~
+      ("reader" -> ("type" -> "squad tagging") ~ ("tokenize" -> tokenize)) ~
+      ("input file" -> inputFile) ~
+      ("output files" -> outputFiles)
+  }
+
+  def squadTaggingDataset(
+    squadDir: String,
+    split: String,
+    version: String="1.1",
+    tokenize: Boolean=true
+  ): JValue = {
+    val file = squadTaggingFile(squadDir, split, version, tokenize)
+    ("data files" -> List(file))
+  }
+
   val baseDir = "/efs/data/dlfa/squad/"
 
   val trainFile = squadFile(baseDir, "train")
   val trainDataset = squadDataset(baseDir, "train")
   val devFile = squadFile(baseDir, "dev")
   val devDataset = squadDataset(baseDir, "dev")
+
+  val taggingTrainFile = squadTaggingFile(baseDir, "train")
+  val taggingTrainDataset = squadTaggingDataset(baseDir, "train")
+  val taggingDevFile = squadTaggingFile(baseDir, "dev")
+  val taggingDevDataset = squadTaggingDataset(baseDir, "dev")
 }
